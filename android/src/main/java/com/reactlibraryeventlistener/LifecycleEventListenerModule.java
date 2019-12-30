2548814b00340ac71504601e5936f712223249dc
Shad Mirza <iamshadmirza@gmail.com>
2019-12-30 11:05:28 +0530

    one React method added

--- android/src/main/java/com/reactlibraryeventlistener/LifecycleEventListenerModule.java
+++ android/src/main/java/com/reactlibraryeventlistener/LifecycleEventListenerModule.java
@@ -1,9 +1,15 @@
 package com.reactlibraryeventlistener;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.FeatureInfo;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.ReactMethod;
 
 public class LifecycleEventListenerModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
@@ -20,18 +26,27 @@ public class LifecycleEventListenerModule extends ReactContextBaseJavaModule imp
         return "LifecycleEventListener";
     }
 
+    @ReactMethod
+    public void showAlertToast(String text) {
+        Context context = getReactApplicationContext();
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+    }
+
     @Override
     public void onHostResume() {
-        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onResume", null);
+        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onResume",
+                null);
     }
 
     @Override
     public void onHostPause() {
-        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onPause", null);
+        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onPause",
+                null);
     }
 
     @Override
     public void onHostDestroy() {
-        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onDestroy", null);
+        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onDestroy",
+                null);
     }
 }
